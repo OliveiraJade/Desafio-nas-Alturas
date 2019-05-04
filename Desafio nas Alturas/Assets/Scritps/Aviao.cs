ae7cbb3a6ec1cdb7526b738700947cb590e0e182
oliveirajade <oliveiradepaulajaqueline@gmail.com>
2019-05-03 22:00:09 -0300

    Adicionando Fisica

---
+++ Desafio nas Alturas/Assets/Scritps/Aviao.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aviao : MonoBehaviour
+{
+    Rigidbody2D fisica;
+
+    private void Awake()
+    {
+        Debug.Log("Acordei");
+        this.fisica = GetComponent<Rigidbody2D>();
+    }
+
+    private void Impulsionar()
+    {
+        this.fisica.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Clicou");
+            Impulsionar();
+        }
+    }
+}
